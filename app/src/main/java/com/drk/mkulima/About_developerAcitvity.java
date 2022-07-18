f9d6053f4f226035c88cc7a7f45707afe7f54ad7
DerrickKirimi <remmydren@gmail.com>
2022-07-18 23:28:50 +0300

    Production

---
+++ app/src/main/java/com/drk/mkulima/About_developerAcitvity.java
@@ -0,0 +1,14 @@
+package com.drk.mkulima;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class About_developerAcitvity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_developer_acitvity);
+    }
+}
